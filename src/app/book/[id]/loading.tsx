a75e27f1230567194934ee887255f762a67fd9ec
Septian A Tama <sfirst.tama@gmail.com>
2023-09-03 02:12:01 +0700

    adjust text position on loading screen

--- src/app/book/[id]/loading.tsx
+++ src/app/book/[id]/loading.tsx
@@ -3,7 +3,7 @@ import { HeadingText } from '~/components/heading-text';
 export default function Loading() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <HeadingText as="h1" className="animate-pulse text-4xl">
+      <HeadingText as="h1" className="mb-8 animate-pulse text-4xl">
         Getting your book from the shelf...
       </HeadingText>
     </div>
